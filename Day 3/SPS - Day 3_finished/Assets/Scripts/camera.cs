1cf3ad3b3b609da41f15036378de8e0e8ac62f6e
Božidar <bozidar.labas@hotmail.com>
2015-03-03 16:22:24 -0800

    Day 3
    Camera scripting, added Environment, parallax background scripting,
    tiling scripting

---
+++ Day 3/SPS - Day 3_finished/Assets/Scripts/camera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+	
+	public GameObject target;
+	public float xOffset = 0;
+	public float yOffset = 0;
+	public float zOffset = 0;
+	
+	void LateUpdate() {
+		this.transform.position = new Vector3(target.transform.position.x + xOffset,
+		                                      transform.position.y,
+		                                      target.transform.position.z + transform.position.z);
+	}
+}
